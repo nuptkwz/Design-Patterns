8efe3bad2ab3d39645633b5a99842812ad4cd24f
keweizhou <1514991931@qq.com>
2020-07-29 23:34:30 +0800

    add adapter

---
+++ design-patterns/src/main/java/com/designpatterns/designpatterns/adapter/Main.java
@@ -0,0 +1,32 @@
+package com.designpatterns.designpatterns.adapter;
+
+import org.springframework.util.StringUtils;
+
+import java.io.*;
+
+/**
+ * Description
+ * 适配器模式
+ * 如果一个类不能访问另一个类的时候，中间加一个转换，就叫转换器
+ * Date 2020/7/29 23:10
+ * Created by kwz
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = null;
+        try {
+            FileInputStream fileInputStream = new FileInputStream("D:/test.txt");
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+            bufferedReader = new BufferedReader(inputStreamReader);
+            String line = bufferedReader.readLine();
+            while (!StringUtils.isEmpty(line)) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            bufferedReader.close();
+        }
+    }
+}
