3490de648ce4b4686139572502019b4070eaff14
keweizhou <1514991931@qq.com>
2020-08-07 23:56:27 +0800

    proxy v3

---
+++ design-patterns/src/main/java/com/designpatterns/designpatterns/proxy/v3/Tank.java
@@ -0,0 +1,47 @@
+package com.designpatterns.designpatterns.proxy.v3;
+
+import java.util.Random;
+
+/**
+ * Description
+ * 问题：我想记录坦克的移动时间
+ * 最简单的办法：修改代码，记录时间
+ * 问题2：如果无法改变方法源码呢？
+ * 用继承？
+ * Date 2020/8/6 23:46
+ * Created by kwz
+ */
+public class Tank implements Movable {
+
+    /**
+     * 模拟坦克移动了一段儿时间
+     */
+    @Override
+    public void move() {
+        System.out.println("Tank moving ......");
+        try {
+            Thread.sleep(new Random().nextInt(10000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Tank2().move();
+    }
+}
+
+class Tank2 extends Tank {
+
+    @Override
+    public void move() {
+        long start = System.currentTimeMillis();
+        super.move();
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
+    }
+}
+
+interface Movable {
+    void move();
+}
