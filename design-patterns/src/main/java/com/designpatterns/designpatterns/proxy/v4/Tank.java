1cf2a43287f4dfaf76848cac3d89a0159e9a4047
keweizhou <1514991931@qq.com>
2020-08-09 23:53:48 +0800

    add proxy v4

---
+++ design-patterns/src/main/java/com/designpatterns/designpatterns/proxy/v4/Tank.java
@@ -0,0 +1,57 @@
+package com.designpatterns.designpatterns.proxy.v4;
+
+/**
+ * Description
+ * Date 2020/8/9 23:52
+ * Created by kwz
+ */
+
+import java.util.Random;
+
+/**
+ * 问题：我想记录坦克的移动时间
+ * 最简单的办法：修改代码，记录时间
+ * 问题2：如果无法改变方法源码呢？
+ * 用继承？
+ * v05:使用代理
+ */
+public class Tank implements Movable {
+
+    /**
+     * 模拟坦克移动了一段儿时间
+     */
+    @Override
+    public void move() {
+        System.out.println("Tank moving ...");
+        try {
+            Thread.sleep(new Random().nextInt(10000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new TankTimeProxy(new Tank()).move();
+    }
+}
+
+class TankTimeProxy implements Movable {
+
+    private Tank tank;
+
+    TankTimeProxy(Tank tank) {
+        this.tank = tank;
+    }
+
+    @Override
+    public void move() {
+        long start = System.currentTimeMillis();
+        tank.move();
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
+    }
+}
+
+interface Movable {
+    void move();
+}
