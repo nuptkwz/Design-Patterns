b20996a6ea5c1a346c39aceba64c4ad43a93ad23
keweizhou <1514991931@qq.com>
2020-08-09 23:59:57 +0800

    add proxy v6

---
+++ design-patterns/src/main/java/com/designpatterns/designpatterns/proxy/v6/Tank.java
@@ -0,0 +1,71 @@
+package com.designpatterns.designpatterns.proxy.v6;
+
+/**
+ * Description
+ * Date 2020/8/9 23:59
+ * Created by kwz
+ */
+public class Tank implements Movable {
+
+    /**
+     * 模拟坦克移动了一段儿时间
+     */
+    @Override
+    public void move() {
+        System.out.println("Tank moving claclacla...");
+        try {
+            Thread.sleep(new Random().nextInt(10000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Tank t = new Tank();
+        TankTimeProxy ttp = new TankTimeProxy(t);
+        TankLogProxy tlp = new TankLogProxy(ttp);
+        tlp.move();
+
+//        new TankLogProxy(
+//                new TankTimeProxy(
+//                        new Tank()
+//                )
+//        ).move();
+    }
+}
+
+class TankTimeProxy implements Movable {
+    Movable m;
+
+    public TankTimeProxy(Movable m) {
+        this.m = m;
+    }
+
+    @Override
+    public void move() {
+        long start = System.currentTimeMillis();
+        m.move();
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
+    }
+}
+
+class TankLogProxy implements Movable {
+    Movable m;
+
+    public TankLogProxy(Movable m) {
+        this.m = m;
+    }
+
+    @Override
+    public void move() {
+        System.out.println("start moving...");
+        m.move();
+        long end = System.currentTimeMillis();
+        System.out.println("stopped!");
+    }
+}
+
+interface Movable {
+    void
