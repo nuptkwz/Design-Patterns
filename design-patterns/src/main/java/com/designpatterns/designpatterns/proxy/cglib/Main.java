6cfbe4248e056e32519da447ab393710148b5dfc
keweizhou <1514991931@qq.com>
2020-08-16 17:49:13 +0800

    add cglib note

--- design-patterns/src/main/java/com/designpatterns/designpatterns/proxy/cglib/Main.java
+++ design-patterns/src/main/java/com/designpatterns/designpatterns/proxy/cglib/Main.java
@@ -10,13 +10,14 @@ import java.util.Random;
 /**
  * Description
  * CGLIB 实现动态代理不需要接口
+ * 它的底层也是用的asm
  * Date 2020/8/16 17:30
  * Created by kwz
  */
 public class Main {
 
     public static void main(String[] args) {
-        //enhancer 英文是增强的意思
+        //enhancer 英文是增强的意思、增强者，增强器，把原来的类增强一下
         Enhancer enhancer = new Enhancer();
         enhancer.setSuperclass(Tank.class);
         enhancer.setCallback(new TimeMethodInterceptor());
@@ -29,6 +30,7 @@ class TimeMethodInterceptor implements MethodInterceptor {
 
     @Override
     public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+        //它的父类打印出来是com.designpatterns.designpatterns.proxy.cglib.Tank，生成tank类的子类
         System.out.println(o.getClass().getSuperclass().getName());
         System.out.println("before");
         Object result = methodProxy.invokeSuper(o, objects);
@@ -38,7 +40,7 @@ class TimeMethodInterceptor implements MethodInterceptor {
 }
 
 class Tank {
-    public void remove() {
+    void remove() {
         System.out.println("Tank moving......");
         try {
             Thread.sleep(new Random().nextInt(10000));
