67e14a0855ba1419707f1256710b3d987efb30e8
keweizhou <1514991931@qq.com>
2021-01-15 23:28:51 +0800

    add DispatcherExecutor

---
+++ design-patterns/src/main/java/com/designpatterns/designpatterns/reactor/DispatcherExecutor.java
@@ -0,0 +1,19 @@
+package com.designpatterns.designpatterns.reactor;
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * Description
+ * Date 2021/1/15 23:26
+ * Created by kwz
+ */
+@Component
+public class DispatcherExecutor {
+
+    @PostConstruct
+    public void autoRegister(){
+
+    }
+}
